35c3cf3677d55608dbbb4a637b67e4562d2e5144
Raymond <20248577+javaarchive@users.noreply.github.com>
2021-07-07 14:04:22 -0700

    Update all.js

--- tests/all.js
+++ tests/all.js
@@ -10,7 +10,7 @@ function wait(ms = 1000){
 }
 
 function launchMongo(){
-  let mongoProc = spawn_command_async("mongo",["--dbpath",process.cwd,"--logpath","/tmp/mongod.log"]);
+  let mongoProc = spawn_command_async("mongod",["--dbpath",process.cwd,"--logpath","/tmp/mongod.log"]);
   mongoProc.stdout.pipe(process.stdout);
   mongoProc.stderr.pipe(process.stdout);
   return mongoProc;
