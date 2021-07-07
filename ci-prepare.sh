0b814c90c66bd219540240d1f0425afd9cb91d46
Raymond <20248577+javaarchive@users.noreply.github.com>
2021-07-07 14:10:05 -0700

    Update ci-prepare.sh

--- ci-prepare.sh
+++ ci-prepare.sh
@@ -1,8 +1,8 @@
 echo "Installing mongodb via tar"
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.4.6.tgz
 tar -xzvf mongodb-linux-x86_64-ubuntu1804-4.4.6.tgz
-ls
 cp mongodb-linux-x86_64-ubuntu1804-4.4.6/bin/mongod mongod
 cp mongodb-linux-x86_64-ubuntu1804-4.4.6/bin/mongo mongo
 chmod +x mongod
 chmod +x mongo
+ls
