b8c497fa4b32d5f99f16da0f03087699445ffb18
Raymond <20248577+javaarchive@users.noreply.github.com>
2021-07-07 13:41:21 -0700

    Create ci-prepare.sh

---
+++ ci-prepare.sh
@@ -0,0 +1,4 @@
+echo "Installing mongodb via tar"
+wget https://fastdl.mongodb.org/linux/mongodb-shell-linux-x86_64-ubuntu1804-4.4.6.tgz
+tar -xzvf mongodb-shell-linux-x86_64-ubuntu1804-4.4.6.tgz
+ls
